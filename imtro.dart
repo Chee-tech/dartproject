6e4e4bcd21f835bd9dec709162e0a36e1604ec7c
JohnpaulChigozie <JohnpaulChigozie7@gmail.com>
2022-07-18 14:50:13 +0100

    Class work 4

--- imtro.dart
+++ imtro.dart
@@ -41,23 +41,23 @@ main(){
 //     print("Even");
 //   }
 //The first Assignment
-String number1="Give us a number";
-print(number1);
-int? numV = int.parse(stdin.readLineSync()!);
-
-if(numV >= 0){
-  print("$numV is a Positve Number");
-}
-else{
-  print("$numV is a Negative Number");
-}
- print("Solving Quadratic Equations");
-print("What is your a value");
-num? a = int.parse(stdin.readLineSync()!);
-print("What is your b value");
-num? b = int.parse(stdin.readLineSync()!);
-print("What is your c value");
-num? c = int.parse(stdin.readLineSync()!);
+// String number1="Give us a number";
+// print(number1);
+// int? numV = int.parse(stdin.readLineSync()!);
+//
+// if(numV >= 0){
+//   print("$numV is a Positve Number");
+// }
+// else{
+//   print("$numV is a Negative Number");
+// }
+//  print("Solving Quadratic Equations");
+// print("What is your a value");
+// num? a = int.parse(stdin.readLineSync()!);
+// print("What is your b value");
+// num? b = int.parse(stdin.readLineSync()!);
+// print("What is your c value");
+// num? c = int.parse(stdin.readLineSync()!);
 // num d = -b;
 // num e = b*b;
 // num f = 4*a*c;
@@ -65,8 +65,74 @@ num? c = int.parse(stdin.readLineSync()!);
 // num h = e-f;
 // double squ = sqrt;
 // print("Your X1 is $d $e $f $squ");
-num x1 = (-b + sqrt((b*b)-4*a*c)) /2*a;
-num x2 = (-b - sqrt((b*b)-4*a*c)) /2*a;
 
-print("Your X1 is $x1 \n Your X2 is $x2");
+
+// For Loop Statement
+
+// for (var i = 0; i<=10; i++){
+//  // print(i);
+// }
+// // For..in Loop Statement
+// var bodyName = [10,20,30,30,40];
+//
+// for(var i  in bodyName )
+// {
+//  // print(i);
+// }
+//
+// // Example for "While loop"
+// int i = 1;
+// while(i <=5)
+// {
+//   // print(i);
+//   // i++;
+// }
+
+// Example of do-while loop
+
+// int a = 20;
+// print("Loop Started");
+// do{
+//   print(a);
+//   a++;
+// }while(a<=30);
+//   print("Loop Ended");
+// print(addTwoNumbers(2, 3));
+
+// var una = HelloWorld();
+// una.addTwoNumbers(4, 20);
+
+print(myName("My name is Chigozie Johnpaul"));
+print(areaOfCircle(10));
+}
+
+// function with Parameters
+
+// int addTwoNumbers(int a, int b,){
+//   int c;
+//   c = a + b;
+//   return c;
+// }
+//
+// class HelloWorld{
+//
+//   int addTwoNumbers(int a, int b,){
+//     int c;
+//     c = a + b;
+//     return c;
+//   }
+// }
+
+
+//Class work
+String myName(String name){
+  String c = name;
+
+  return c;
+}
+
+int areaOfCircle(double b,){
+  double c;
+  c = pi * b*b;
+  return c.toInt();
 }
